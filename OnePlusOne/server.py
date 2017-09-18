031bd02607ea868e27af50179f615b228496dfaa
Bluice Zhen <bluice.zhen@gmail.com>
2017-09-18 17:20:09 +0800

    fetch: route

--- OnePlusOne/server.py
+++ OnePlusOne/server.py
@@ -1,7 +1,23 @@
-from werkzeug.wrappers import Response
+from typing import List
+from werkzeug.exceptions import HTTPException
+from werkzeug.wrappers import Response, Request
+from werkzeug.routing import Map, Rule
 
 
 class Server(object):
     def __call__(self, environ, start_response):
-        response = Response('Hello World!')
+        request = Request(environ)
+        response = self.dispatch_request(request)
         return response(environ, start_response)
+
+    def __init__(self, route_l: List[Rule]):
+        self.url_map = Map(route_l)
+        self.response_header = []
+
+    def dispatch_request(self, request):
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            resource = adapter.match()[0]()
+            return Response(resource.get(), mimetype="text/plain")
+        except HTTPException as e:
+            return e
--- demo/demo.py
+++ demo/demo.py
@@ -1,5 +1,12 @@
-from OnePlusOne import App
+from OnePlusOne import App, Resource
+
+
+class ResourceHelloWorld(Resource):
+    def get(self):
+        return "hello world"
+
 
 if __name__ == "__main__":
     app = App()
+    app.add_resource("/hello_world", ResourceHelloWorld)
     app.run()
